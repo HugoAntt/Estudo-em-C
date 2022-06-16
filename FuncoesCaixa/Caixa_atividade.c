e597886be9d80048005758b371d295210d058219
Hugo Antunes <86171824+HugoAntt@users.noreply.github.com>
2022-06-16 18:13:58 -0300

    Resolução de Problemas

--- FuncoesCaixa/Caixa_atividade.c
+++ FuncoesCaixa/Caixa_atividade.c
@@ -1,4 +1,4 @@
-/*É muito comum a escolha de opções em sistemas, como por exemplo, em menus para sistemas de caixas de autoatendimento 24 horas. Descreva uma situação problema da sua escolha, que tenha a escolha de uma opção dentre umas 5 a 10, faça o algoritmo ( fluxograma ) e implemente o código com uso de funções. Podem ser funções simples que executem simples operações e/ou mostrem mensagens/resultados.
+/*É muito comum a escolha de opções em sistemas, como por exemplo, em menus para sistemas de caixas de autoatendimento 24 horas. Descreva uma situação problema da sua escolha, que tenha a escolha de uma opção dentre umas 5 a 10, e implemente o código com uso de funções. Podem ser funções simples que executem simples operações e/ou mostrem mensagens/resultados.
 */
 
 #include <stdio.h>
